cc116c7a28912bd304b4272284f54fea5c17ebee
Olivera2708 <olivera.radovanovic2002@gmail.com>
2023-11-09 19:05:25 +0100

    added search part

--- app/src/main/java/com/example/bookify/ResultsActivity.java
+++ app/src/main/java/com/example/bookify/ResultsActivity.java
@@ -1,6 +1,7 @@
 package com.example.bookify;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.Pair;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -12,17 +13,49 @@ import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ResultsActivity extends AppCompatActivity {
 
     FloatingActionButton filterButton;
+    Button editDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        editDate = findViewById(R.id.editButton);
+        editDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MaterialDatePicker<Pair<Long, Long>> materialDatePicker = MaterialDatePicker.Builder.dateRangePicker().setSelection(new Pair<>(
+                        MaterialDatePicker.thisMonthInUtcMilliseconds(),
+                        MaterialDatePicker.todayInUtcMilliseconds()
+                )).build();
+
+                materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Pair<Long, Long>>() {
+                    @Override
+                    public void onPositiveButtonClick(Pair<Long, Long> selection) {
+                        String startDate = new SimpleDateFormat("dd.MM.yyyy.", Locale.getDefault()).format(new Date(selection.first));
+                        String endDate = new SimpleDateFormat("dd.MM.yyyy.", Locale.getDefault()).format(new Date(selection.second));
+
+                        editDate.setText(startDate + " - " + endDate);
+                    }
+                });
+
+                materialDatePicker.show(getSupportFragmentManager(), "tag");
+            }
+        });
 
         filterButton = (FloatingActionButton) findViewById(R.id.filterButton);
         filterButton.setOnClickListener(new View.OnClickListener() {
