2632b2fbafb015d84b18ae1d1a3d3f8d9e86d01a
Olivera2708 <olivera.radovanovic2002@gmail.com>
2023-11-10 21:33:54 +0100

    done with image slider

--- app/src/main/java/com/example/bookify/AccommodationDetailsActivity.java
+++ app/src/main/java/com/example/bookify/AccommodationDetailsActivity.java
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.util.Pair;
 
 import android.os.Bundle;
+import android.transition.Slide;
 import android.view.View;
 import android.widget.Button;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.Locale;
 
 public class AccommodationDetailsActivity extends AppCompatActivity {
 
     Button reservationDate;
+    ImageSlider imageSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,5 +52,17 @@ public class AccommodationDetailsActivity extends AppCompatActivity {
                 materialDatePicker.show(getSupportFragmentManager(), "tag");
             }
         });
+
+        imageSlider = findViewById(R.id.imageSlider);
+        ArrayList<SlideModel> imageList = new ArrayList<>();
+
+        imageList.add(new SlideModel(R.drawable.apartman1, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.apartman2, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.apartman3, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.apartman4, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.apartman5, ScaleTypes.FIT));
+        imageList.add(new SlideModel(R.drawable.apartman6, ScaleTypes.FIT));
+
+        imageSlider.setImageList(imageList, ScaleTypes.FIT);
     }
 }
