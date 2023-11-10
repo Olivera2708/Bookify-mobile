79ade45d16ec504b5c84ba361c8e482aad183705
Olivera2708 <olivera.radovanovic2002@gmail.com>
2023-11-10 20:38:10 +0100

    save before adding image slider

--- app/src/main/java/com/example/bookify/AccommodationDetailsActivity.java
+++ app/src/main/java/com/example/bookify/AccommodationDetailsActivity.java
@@ -3,11 +3,9 @@ package com.example.bookify;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.util.Pair;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
 import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 
@@ -25,7 +23,7 @@ public class AccommodationDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_accommodation_details);
 
         View reservationTile = findViewById(R.id.reservation);
-        Button reservationDate = reservationTile.findViewById(R.id.editDate);
+        reservationDate = reservationTile.findViewById(R.id.editDate);
         reservationDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
