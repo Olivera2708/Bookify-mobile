18f57d25f439a222b4cf5200d61c188610fb480a
Olivera2708 <olivera.radovanovic2002@gmail.com>
2023-11-10 22:57:56 +0100

    done with landing page

---
+++ app/src/main/java/com/example/bookify/LandingActivity.java
@@ -0,0 +1,59 @@
+package com.example.bookify;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.util.Pair;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class LandingActivity extends AppCompatActivity {
+    Button editDate;
+    Button search;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        View searchLayout = findViewById(R.id.searchLayout);
+        editDate = searchLayout.findViewById(R.id.dateInput);
+        editDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MaterialDatePicker<Pair<Long, Long>> materialDatePicker = MaterialDatePicker.Builder.dateRangePicker().setSelection(new Pair<>(
+                        MaterialDatePicker.thisMonthInUtcMilliseconds(),
+                        MaterialDatePicker.todayInUtcMilliseconds()
+                )).build();
+
+                materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Pair<Long, Long>>() {
+                    @Override
+                    public void onPositiveButtonClick(Pair<Long, Long> selection) {
+                        String startDate = new SimpleDateFormat("dd.MM.yyyy.", Locale.getDefault()).format(new Date(selection.first));
+                        String endDate = new SimpleDateFormat("dd.MM.yyyy.", Locale.getDefault()).format(new Date(selection.second));
+
+                        editDate.setText(startDate + " - " + endDate);
+                    }
+                });
+
+                materialDatePicker.show(getSupportFragmentManager(), "tag");
+            }
+        });
+
+        search = searchLayout.findViewById(R.id.searchButton);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LandingActivity.this, ResultsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
