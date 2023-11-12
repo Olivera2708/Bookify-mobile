56c2a0667fb255672dc65cf7b7d80abf9770b00f
Milos Bojanic <mbojanic53@gmail.com>
2023-11-13 00:04:34 +0100

    Added activity for adding new accommodations and editing accommodations

---
+++ app/src/main/java/com/example/bookify/AccommodationUpdateActivity.java
@@ -0,0 +1,14 @@
+package com.example.bookify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccommodationUpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accommodation_update);
+    }
+}
