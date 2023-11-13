72903440e4020bb129def880dc05066ffe6c4d20
Olivera2708 <olivera.radovanovic2002@gmail.com>
2023-11-13 16:47:33 +0100

    added listener

--- app/src/main/java/com/example/bookify/ResultsActivity.java
+++ app/src/main/java/com/example/bookify/ResultsActivity.java
@@ -18,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -26,6 +27,7 @@ import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.slider.RangeSlider;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -107,6 +109,30 @@ public class ResultsActivity extends AppCompatActivity {
         final BottomSheetDialog dialog = new BottomSheetDialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.filter);
+
+        EditText minPrice = dialog.findViewById(R.id.minPrice);
+        minPrice.setText("0", TextView.BufferType.EDITABLE);
+        EditText maxPrice = dialog.findViewById(R.id.maxPrice);
+        maxPrice.setText("100", TextView.BufferType.EDITABLE);
+
+        RangeSlider priceSlider = dialog.findViewById(R.id.priceBar);
+        priceSlider.addOnSliderTouchListener(new RangeSlider.OnSliderTouchListener() {
+            @Override
+            public void onStartTrackingTouch(@NonNull RangeSlider slider) {
+                float valueMin = slider.getValues().get(0);
+                minPrice.setText((int) valueMin);
+
+                float valueMax = slider.getValues().get(0);
+                maxPrice.setText((int) valueMax);
+            }
+
+            @Override
+            public void onStopTrackingTouch(@NonNull RangeSlider slider) {
+
+            }
+        });
+
+
 
         String[] sort = new String[]{"Price lowest first", "Price highest first", "Name"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.dropdown_item, sort);
