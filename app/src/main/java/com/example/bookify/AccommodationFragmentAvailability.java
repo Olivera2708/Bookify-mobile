711bb60356207d7b1a5e707884a28a0b4aacf799
Milos Bojanic <mbojanic53@gmail.com>
2023-11-13 11:21:12 +0100

    Added table view and adding rows

--- app/src/main/java/com/example/bookify/AccommodationFragmentAvailability.java
+++ app/src/main/java/com/example/bookify/AccommodationFragmentAvailability.java
@@ -2,11 +2,26 @@ package com.example.bookify;
 
 import android.os.Bundle;
 
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.util.Pair;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +29,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class AccommodationFragmentAvailability extends Fragment {
+
+    private TableLayout tableLayout;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +76,85 @@ public class AccommodationFragmentAvailability extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_accommodation_availability, container, false);
+        View view = inflater.inflate(R.layout.fragment_accommodation_availability, container, false);
+
+        Button dates = view.findViewById(R.id.datesInput);
+        dates.setOnClickListener(v -> {
+            MaterialDatePicker<Pair<Long, Long>> materialDatePicker = MaterialDatePicker.Builder.dateRangePicker().setSelection(new Pair<>(
+                    MaterialDatePicker.thisMonthInUtcMilliseconds(),
+                    MaterialDatePicker.todayInUtcMilliseconds()
+            )).build();
+
+            materialDatePicker.addOnPositiveButtonClickListener(new MaterialPickerOnPositiveButtonClickListener<Pair<Long, Long>>() {
+                @Override
+                public void onPositiveButtonClick(Pair<Long, Long> selection) {
+                    String startDate = new SimpleDateFormat("dd.MM.yyyy.", Locale.getDefault()).format(new Date(selection.first));
+                    String endDate = new SimpleDateFormat("dd.MM.yyyy.", Locale.getDefault()).format(new Date(selection.second));
+
+                    dates.setText(startDate + " - " + endDate);
+                }
+            });
+
+            materialDatePicker.show(getActivity().getSupportFragmentManager(), "tag");
+        });
+
+        tableLayout = view.findViewById(R.id.tableLayout);
+
+        TextInputEditText price = view.findViewById(R.id.priceInput);
+
+        Button add = view.findViewById(R.id.btnAdd);
+        add.setOnClickListener(v -> {
+            String checkDates = dates.getText().toString();
+            String priceTxt = price.getText().toString();
+            addRowWithData(checkDates, priceTxt);
+        });
+
+        return view;
+    }
+
+    private void addRowWithData(String data1, String data2) {
+        TableRow tableRow = new TableRow(getActivity());
+
+        tableRow.setBackground(ResourcesCompat.getDrawable(getResources() ,R.drawable.border_background, null));
+
+        // Set TableRow properties (optional)
+        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT
+        );
+        tableRow.setLayoutParams(layoutParams);
+
+        // Create TextView for Data 1
+        TextView textViewData1 = createTextView(data1);
+        // Create TextView for Data 2
+        TextView textViewData2 = createTextView(data2);
+
+        TextView textViewData3 = createTextView("Delete");
+
+        // Add TextViews to TableRow
+        tableRow.addView(textViewData1);
+        tableRow.addView(textViewData2);
+        tableRow.addView(textViewData3);
+
+        // Add TableRow to TableLayout
+        tableLayout.addView(tableRow);
+    }
+
+    // Method to create a TextView
+    private TextView createTextView(String text) {
+        TextView textView = new TextView(getActivity());
+        textView.setBackground(ResourcesCompat.getDrawable(getResources() ,R.drawable.border_background, null));
+
+        // Set TextView properties (optional)
+        TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(
+                TableRow.LayoutParams.WRAP_CONTENT,
+                TableRow.LayoutParams.WRAP_CONTENT
+        );
+        textView.setLayoutParams(layoutParams);
+        textView.setPadding(16, 8, 16, 8);
+        textView.setText(text);
+        textView.setGravity(Gravity.CENTER);
+
+        return textView;
     }
 }
