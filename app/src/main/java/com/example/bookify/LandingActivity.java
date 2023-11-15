f770d723811a499b7b7f11500d0adff8e523249a
Milos Bojanic <mbojanic53@gmail.com>
2023-11-16 00:06:22 +0100

    Delete

--- app/src/main/java/com/example/bookify/LandingActivity.java
+++ app/src/main/java/com/example/bookify/LandingActivity.java
@@ -75,23 +75,8 @@ public class LandingActivity extends AppCompatActivity {
             }
         });
 
-        View accoLayout = findViewById(R.id.acco1);
+        View accoLayout = findViewById(R.id.acco4);
         Button details = accoLayout.findViewById(R.id.details);
-        details.setOnClickListener(v -> {
-        });
-
-        accoLayout = findViewById(R.id.acco2);
-        details = accoLayout.findViewById(R.id.details);
-        details.setOnClickListener(v -> {
-        });
-
-        accoLayout = findViewById(R.id.acco3);
-        details = accoLayout.findViewById(R.id.details);
-        details.setOnClickListener(v -> {
-        });
-
-        accoLayout = findViewById(R.id.acco4);
-        details = accoLayout.findViewById(R.id.details);
         details.setOnClickListener(v -> {
             Intent intent = new Intent(LandingActivity.this, ReportsActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
