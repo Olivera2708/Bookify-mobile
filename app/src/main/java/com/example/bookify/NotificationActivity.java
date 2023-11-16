5bccddaf2619ef243ed44c434fa2537161a91289
Olivera2708 <olivera.radovanovic2002@gmail.com>
2023-11-16 17:17:12 +0100

    new toggle for notification settings

--- app/src/main/java/com/example/bookify/NotificationActivity.java
+++ app/src/main/java/com/example/bookify/NotificationActivity.java
@@ -2,9 +2,14 @@ package com.example.bookify;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 
 public class NotificationActivity extends AppCompatActivity {
 
@@ -17,10 +22,23 @@ public class NotificationActivity extends AppCompatActivity {
         buttonSettings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(NotificationActivity.this, NotificationSettingsActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(NotificationActivity.this, NotificationSettingsActivity.class);
+//                startActivity(intent);
+                showDialog();
             }
         });
+    }
+
+    private void showDialog() {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.settings);
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.CENTER_VERTICAL);
     }
 
 }
