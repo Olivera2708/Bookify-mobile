4d426f2b80e7d60895f14f2e4dbb62a51e4edfa8
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2023-11-16 19:37:58 +0100

    Add implementation of navigation bars for different roles

---
+++ app/src/main/java/com/example/bookify/NavigationBar.java
@@ -0,0 +1,10 @@
+package com.example.bookify;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public abstract class NavigationBar {
+        public static void setNavigationBar(BottomNavigationView navigationView) {
+                // TO-DO
+
+        }
+}
