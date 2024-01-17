2e075834dc2eb12f02c59347709835ebf79d4142
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2024-01-17 21:15:19 +0100

    Minor bugifx

--- app/src/main/java/com/example/bookify/activities/user/AccountDetailsActivity.java
+++ app/src/main/java/com/example/bookify/activities/user/AccountDetailsActivity.java
@@ -242,6 +242,12 @@ public class AccountDetailsActivity extends AppCompatActivity {
             progres3.setProgress(ratingDTO.getThreeStars() * 100 / count);
             progres2.setProgress(ratingDTO.getTwoStars() * 100 / count);
             progres1.setProgress(ratingDTO.getOneStars() * 100 / count);
+        } else {
+            progres5.setProgress(0);
+            progres4.setProgress(0);
+            progres3.setProgress(0);
+            progres2.setProgress(0);
+            progres1.setProgress(0);
         }
 
         total_1.setText("(" + ratingDTO.getOneStars() + ")");
