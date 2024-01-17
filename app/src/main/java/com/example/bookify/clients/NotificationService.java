75708ac9773c316ad685a502be46b543ecebe546
Aleksa Vukomanovic <vukomanovic.aleksa@gmail.com>
2024-01-17 19:44:24 +0100

    Add settings displaying and changing feature

--- app/src/main/java/com/example/bookify/clients/NotificationService.java
+++ app/src/main/java/com/example/bookify/clients/NotificationService.java
@@ -1,12 +1,15 @@
 package com.example.bookify.clients;
 
 import com.example.bookify.model.NotificationDTO;
+import com.example.bookify.model.NotificationSettingsDTO;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface NotificationService {
@@ -17,4 +20,18 @@ public interface NotificationService {
     })
     @GET("notifications/{userId}")
     Call<List<NotificationDTO>> getUserNotifications(@Path("userId") Long userId);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type:application/json"
+    })
+    @GET("notifications/{userId}/settings")
+    Call<NotificationSettingsDTO> getUserNotificationSettings(@Path("userId") Long userId);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type:application/json"
+    })
+    @PUT("notifications/{userId}/settings")
+    Call<NotificationSettingsDTO> setUserNotificationSettings(@Path("userId") Long userId, @Body NotificationSettingsDTO notificationSettingsDTO);
 }
